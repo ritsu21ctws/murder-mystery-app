123116d930418644d5347439ac652297da6d13dc
ritsu <ritsu.21.ctws@gmail.com>
2025-02-02 17:13:31 +0900

    Jestの導入と設定

---
+++ jest.setup.ts
@@ -0,0 +1,14 @@
+import "@testing-library/jest-dom";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+/* eslint @typescript-eslint/no-explicit-any: 0 */
+if (!global.structuredClone) {
+  global.structuredClone = function structuredClone(objectToClone: any) {
+    if (objectToClone === undefined) return undefined;
+    return JSON.parse(JSON.stringify(objectToClone));
+  };
+}
+
+global.TextEncoder = require("util").TextEncoder;
