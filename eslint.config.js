d49da95d3d65ff7ff9c75a55d3fdcceea7115a9a
ritsu <ritsu.21.ctws@gmail.com>
2025-02-02 17:07:00 +0900

    ESLintの設定ファイルの調整

--- eslint.config.js
+++ eslint.config.js
@@ -3,12 +3,27 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+    },
+  },
+  {
+    ignores: ["dist/*", "src/components/ui/**.tsx"],
+  },
+  {
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
 ];
