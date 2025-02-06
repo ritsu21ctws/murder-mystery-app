69844aae7d2302e09b9ba6813b8f308e11a1937d
ritsu <ritsu.21.ctws@gmail.com>
2025-02-07 00:39:32 +0900

    CIでエラーになる事象の修正

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
 import App from './App.tsx';
+import '@/types/@chakra-ui';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
