4e0e5fc8c22fa852aa6fd8ae5679e190d6665979
ritsu <ritsu.21.ctws@gmail.com>
2025-02-07 00:43:40 +0900

    ビルドエラーになる事象の修正

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
 import App from './App.tsx';
-import '@/types/@chakra-ui';
+import '@/types/@chakra-ui.d.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
