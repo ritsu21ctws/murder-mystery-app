3b479f7c4ebb9b85057326c215f057c019cd0991
ritsu <ritsu.21.ctws@gmail.com>
2025-02-07 13:24:49 +0900

    CI環境でテストエラーになる事象の修正

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import env from 'vite-plugin-env-compatible';
-import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), env({ prefix: 'VITE', mountedPath: 'process.env' })],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
 });
