ff1039f17fda8218dea9343c5f3d1f92eac059d3
ritsu <ritsu.21.ctws@gmail.com>
2025-02-16 19:36:10 +0900

    主催イベント画面に、イベント作成ボタンを表示

--- src/components/pages/HostedEvents.tsx
+++ src/components/pages/HostedEvents.tsx
@@ -1,5 +1,18 @@
 import React, { memo } from 'react';
+import { useNavigate } from 'react-router';
+import { PrimaryButton } from '../atoms/PrimaryButton';
 
 export const HostedEvents: React.FC = memo(() => {
-  return <p>HostedEventsページです</p>;
+  const navigate = useNavigate();
+
+  const onClickCreateEvent = () => {
+    navigate(`../create`);
+  };
+
+  return (
+    <>
+      <p>HostedEventsページです</p>
+      <PrimaryButton onClick={onClickCreateEvent}>イベント作成</PrimaryButton>
+    </>
+  );
 });
